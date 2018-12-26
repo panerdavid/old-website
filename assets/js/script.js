d88442f0c0e1e785745e7b80ddc9c7b26912cb40
David Paner <panerdavid@berkeley.edu>
2018-12-25 22:49:59 -0800

    trying to fix resizing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,15 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
   $('#wrapper').fullpage({
-    anchors:['about', 'my dog'],
-    loopHorizontal: false
+    anchors: ["site"],
+    loopHorizontal: false,
+    controlArrows: false,
+    
 
   });
 
 
-$('#me').typeIt({
+  $('#me').typeIt({
     strings: ["Student", "Musician", "Programmer", "Educator", "Dog lover"],
     speed: 175,
     breakLines: false,
@@ -15,7 +17,8 @@ $('#me').typeIt({
     startDelay: 150,
     deleteDelay: 1000,
     loopDelay: 900
-});
+
+  });
 });
 var slideIndex = 1;
 next(slideIndex);
@@ -34,14 +37,18 @@ function next(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+    slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
