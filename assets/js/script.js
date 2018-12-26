1c9671ab7e81982b381e234a63055d8e6d170cf6
David Paner <panerdavid@berkeley.edu>
2018-12-26 03:55:59 -0800

    replace pics of nina

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     anchors: ["site"],
     loopHorizontal: false,
     controlArrows: false,
-    
+
 
   });
 
@@ -20,35 +20,3 @@ $(document).ready(function() {
 
   });
 });
-var slideIndex = 1;
-next(slideIndex);
-
-
-function plusSlides(n) {
-  next(slideIndex += n);
-}
-
-
-function currentSlide(n) {
-  next(slideIndex = n);
-}
-
-function next(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1
-  }
-  if (n < 1) {
-    slideIndex = slides.length
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-}
